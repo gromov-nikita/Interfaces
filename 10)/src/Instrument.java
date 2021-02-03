e1c06cc700ff942813d3bb041e7937b3bf388c4e
gromov-nikita <hromay.n999@gmail.com>
2021-02-03 16:27:31 +0300

    1

---
+++ 10)/src/Instrument.java
@@ -0,0 +1,7 @@
+public interface Instrument {
+    // Compile-time constant:
+    int VALUE = 5; // static & final
+    //void play(Note n);// Automatically public
+    // Cannot have method definitions:
+    void adjust();
+}
---
+++ 10)/src/Music5.java
@@ -0,0 +1,23 @@
+public class Music5 {
+    // Doesn’t care about type, so new types
+    // added to the system still work right:
+    static void tune(Playable i) {
+        // ...
+        i.play(Note.MIDDLE_C);
+    }
+    static void tuneAll(Playable[] e) {
+        for(Playable i : e)
+            tune(i);
+    }
+    public static void main(String[] args) {
+        // Upcasting during addition to the array:
+        Playable[] orchestra = {
+                new Wind(),
+                new Percussion(),
+                new Stringed(),
+                new Brass(),
+                new Woodwind()
+        };
+        tuneAll(orchestra);
+    }
+}
