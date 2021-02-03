6969caa06f88d232571e069b1d8d3d902f5bba41
gromov-nikita <hromay.n999@gmail.com>
2021-02-03 16:30:04 +0300

    1

--- 10)/src/Music5.java
+++ 10)/src/Music5.java
@@ -1,3 +1,9 @@
+/*
+Exercise 10: (3) Modify Musics.java by adding a Playable interface. Move the play( )
+declaration from Instrument to Playable. Add Playable to the derived classes by
+including it in the implement s list. Change tune( ) so that it takes a Playable instead of
+an Instrument.
+ */
 public class Music5 {
     // Doesn’t care about type, so new types
     // added to the system still work right:
